b47202fde29357ffb4238100b69112b50f53965a
MiddleMan5 <mikelson.public@gmail.com>
2015-08-03 22:56:02 -0700

    Progress into the final Arduino Code
    The work on this project isn't done, but the new included ZIP
    "FirstSuccessfulBuild" contains the successor to this now depreciated
    firmware set. The arduino "CPU" is being traded for the BeagleBoneBlack
    project

--- code/HostControllerFirmware/Onyx-HostFirmware-7-24-15 - Copy/Onyx-HostFirmware-7-24-15.ino
+++
@@ -1,9 +0,0 @@
-/**
-* Project Onyx
-* Master Branch - Integrated Host Firmware
-* Quinn Mikelson - Initial Commit 7/2/2015
-*/
-#include <Onyx.h>
-Onyx MK1;
-void setup(){MK1.setMode(STARTUP);} 
-void loop(){MK1.setMode(STANDBY);}
